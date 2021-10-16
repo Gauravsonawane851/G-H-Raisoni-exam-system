089af927f6f24a695fe0899a43763d659da78240
Gauravsonawane851 <88612982+Gauravsonawane851@users.noreply.github.com>
2021-10-17 01:12:21 +0530

    Add files via upload

---
+++ dbConnection.php
@@ -0,0 +1,5 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+$con= new mysqli('sql211.epizy.com','epiz_30085262','mq0RBKRCXhxJwe','epiz_30085262_examdb')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
